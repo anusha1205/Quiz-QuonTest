1c580794175147059cf5d79f17f72181e2cafbc7
Anusha Gonal <113822918+anusha1205@users.noreply.github.com>
2024-06-04 14:19:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const startBtn = document.querySelector('.start-btn');
+    const choices = document.querySelector('#choices');
+    const startBtns = document.querySelectorAll('.start-btn');
     const popupInfo = document.querySelector('.popup-info');
     const exitBtn = document.querySelector('.go-back');
     const main = document.querySelector('.main');
 
+    choices.addEventListener('change', function () {
+        const selectedValue = this.value;
+        document.querySelectorAll('.subject-div').forEach(div => div.style.display = 'none');
+        if (selectedValue !== 'choice0') {
+            document.querySelector(`.${selectedValue}`).style.display = 'flex';
+        }
+    });
 
-    startBtn.onclick = () => {
-        popupInfo.classList.add('active');
-        main.classList.add('active');
-    }
-    exitBtn.onclick = ()=>{
+    startBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            popupInfo.classList.add('active');
+            main.classList.add('active');
+        });
+    });
+
+    exitBtn.addEventListener('click', () => {
         popupInfo.classList.remove('active');
-        main.classList.remove('active')
-    }
+        main.classList.remove('active');
+    });
 });
